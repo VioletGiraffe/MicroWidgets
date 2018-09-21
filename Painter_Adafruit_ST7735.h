a3918f0d406c542a906ad6c0723189b735c9e684
VioletGiraffe <violet.giraffe@noreply.github.com>
2018-09-21 11:35:09 +0300

    Adafruit ST7735 build fixed

--- Painter_Adafruit_ST7735.h
+++ Painter_Adafruit_ST7735.h
@@ -3,7 +3,10 @@
 #include "Painter_Adafruit_Base.h"
 
 #include <Adafruit_GFX.h>
+#ifndef ST7735_BLACK // Adafruit headers lack an include guard
 #include <Adafruit_ST7735.h>
+#endif
+
 
 class PainterAdafruitST7735 : public PainterAdafruitBase<PainterAdafruitST7735, Adafruit_ST7735>
 {
